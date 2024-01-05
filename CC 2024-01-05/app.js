7c77e7159d5ba1f5816c0b8ad4c8027e2795743b
bfoster4 <beaudfoster@gmail.com>
2024-01-05 11:45:46 -0600

    Add coding challenge from -1-5-24' '' '

---
+++ CC 2024-01-05/app.js
@@ -0,0 +1,11 @@
+// Description:
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+function greeting(input){
+    return 'Hello, ' + input + ' how are you doing today?'
+}
+
+console.log(greeting('Beau'))

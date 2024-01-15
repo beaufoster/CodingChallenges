19f8243f705b499b4107ff44d811d33bb9c7fa56
bfoster4 <beaudfoster@gmail.com>
2024-01-15 09:08:34 -0600

    Add coding challenge for 1-15-24

---
+++ CC 2024-01-15/app.js
@@ -0,0 +1,8 @@
+// Description:
+// Write a function which converts the input string to uppercase.
+
+const upperCase = str =>  str.toUpperCase()
+
+console.log(upperCase('hello'), ['HELLO'])
+console.log(upperCase('test'), ['TEST'])
+console.log(upperCase('winner'), ['WINNER'])

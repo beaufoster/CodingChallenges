64c798e40e51bb2d88e97772e31914edff4cdd6f
bfoster4 <beaudfoster@gmail.com>
2024-01-18 09:25:53 -0600

    Add coding challenge for 1-18-24

---
+++ CC 2024-01-18/app.js
@@ -0,0 +1,17 @@
+// Description:
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+const convertToString = num => num.toString()
+
+
+
+console.log(convertToString(123), ['123'])
+console.log(convertToString(999), ['999'])
+console.log(convertToString(-100), ['-100'])

b2149fbe9297f7810f42988b6ad8040cb88fa9ca
bfoster4 <beaudfoster@gmail.com>
2024-01-21 12:36:55 -0600

    Add coding challenge from 1-21-24

---
+++ CC 2024-01-21/app.js
@@ -0,0 +1,25 @@
+// Description:
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// Example(Input1, Input2 --> Output):
+
+// 6, 10 --> 32
+// 3, 3 --> 9
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+// function rectangleOrSquare(num1, num2){
+//     if(num1 === num2){
+//         return num1 * num2
+//     }else{
+//         return (num1 *2) + (num2 *2)
+//     }
+// }
+
+// ternary code
+
+const rectangleOrSquare = (num1, num2) => num1 === num2 ? num1 * num2 : (num1 *2) + (num2 *2)
+
+
+console.log(rectangleOrSquare(6,10), [32])
+console.log(rectangleOrSquare(3,3), [9])

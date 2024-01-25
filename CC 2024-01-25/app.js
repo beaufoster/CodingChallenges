dfb44c0b2878c25c04c3b8b4c38cab0410bc0f16
bfoster4 <beaudfoster@gmail.com>
2024-01-25 12:55:43 -0600

    Add coding challenge from 1-25-2024

---
+++ CC 2024-01-25/app.js
@@ -0,0 +1,21 @@
+// Description:
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+
+const totalGoals = (Laliga, Copa, Champions) => Laliga + Copa + Champions
+
+console.log(totalGoals(5, 10, 20), [17])
+console.log(totalGoals(4 ,10, 33), [47])
+console.log(totalGoals(1, 1, 1), [3])

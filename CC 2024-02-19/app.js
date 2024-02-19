a04e96108466c909198b0370c32afa01f563a844
bfoster4 <beaudfoster@gmail.com>
2024-02-19 14:10:04 -0600

    Add coding challenge for 2-19-24

---
+++ CC 2024-02-19/app.js
@@ -0,0 +1,8 @@
+// Description:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+const convert = boolean => boolean ? 'Yes' : 'No'
+
+console.log(convert(true), 'Yes')
+console.log(convert(false), 'No')

2975b2a5bc1b44b088039f1bc45579fc1057acf2
bfoster4 <beaudfoster@gmail.com>
2024-04-02 16:48:32 -0500

    Add coding challenge from 4-2-24

---
+++ CC 2024-04-02/app.js
@@ -0,0 +1,7 @@
+// Description:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+const removeExclamationMarks = string => string.replaceAll('!', '')
+
+console.log(removeExclamationMarks('!asdas!'))

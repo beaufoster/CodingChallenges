8f35e81166523d739989308f977df33a2c1e7a32
bfoster4 <beaudfoster@gmail.com>
2024-04-10 11:16:04 -0500

    Add coding challenge from 4-10-24

---
+++ CC 2024-04-10/app.js
@@ -0,0 +1,22 @@
+// Description:
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+
+
+function getCount(str) {
+    let count = 0
+    // str = str.lowerCase()
+    str.split('').map((letter) => {
+        if(letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u'){
+            count ++
+        }
+    })
+    return count
+
+  }
+
+  console.log(getCount("abracadabra"), 5)

b308d34f4ec9f6cefb350ba5f2c2d2722f715383
bfoster4 <beaudfoster@gmail.com>
2024-04-15 11:44:56 -0500

    Add coding challenge from 4-15-24

---
+++ CC 2024-04-15/app.js
@@ -0,0 +1,9 @@
+// Description:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+const square = num => Math.pow(num, 2)
+
+console.log(square(3), 9)
+console.log(square(4), 16)
+console.log(square(5), 25)
+console.log(square(6), 36)
